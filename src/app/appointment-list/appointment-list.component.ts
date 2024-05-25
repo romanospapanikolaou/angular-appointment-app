76f4965f57e3b4c2d6e4e16553d5dde9bea1a36e
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-05-25 17:44:19 +0200

    css

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -4,7 +4,7 @@ import { Appointment } from '../models/appointment';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.css',
+  styleUrls: ['./appointment-list.component.css'],
 })
 export class AppointmentListComponent implements OnInit {
   newAppointmentTitle: string = '';
@@ -12,12 +12,13 @@ export class AppointmentListComponent implements OnInit {
   appointments: Appointment[] = [];
 
   ngOnInit(): void {
-    let savedAppointments = localStorage.getItem('appointments');
+    const savedAppointments = localStorage.getItem('appointments');
     this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
   }
-  addApointment() {
+
+  addAppointment(): void {
     if (this.newAppointmentTitle.trim() && this.newAppointmentDate) {
-      let newAppointment: Appointment = {
+      const newAppointment: Appointment = {
         id: Date.now(),
         title: this.newAppointmentTitle,
         date: this.newAppointmentDate,
@@ -25,12 +26,12 @@ export class AppointmentListComponent implements OnInit {
       this.appointments.push(newAppointment);
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
-      alert(this.appointments.length);
 
       localStorage.setItem('appointments', JSON.stringify(this.appointments));
     }
   }
-  deleteAppointment(index: number) {
+
+  deleteAppointment(index: number): void {
     this.appointments.splice(index, 1);
     localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
